db3974e5ed1ebbb3e0900fce9529059e39010338
mstree <111582436+mysocialtree@users.noreply.github.com>
2022-08-19 20:03:50 +0200

    Update api-call.php

--- api-call.php
+++ api-call.php
@@ -1,1 +1,15 @@
+<?php
+// url https://mstree.de/api/get.php + ? + api=token
+$url = 'https://mstree.de/api/get.php?api=token';
 
+// get data from Server
+$contents = file_get_contents($url);
+
+
+if($contents !== false){
+
+    $character = json_decode($contents);
+    echo $character->id; // get data -> "data"
+
+}
+?>
